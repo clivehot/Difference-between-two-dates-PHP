366544f34e01a0fd96fa85697a025ad85a26c0e2
clivehot <36730246+clivehot@users.noreply.github.com>
2018-09-29 11:39:17 +0200

    Update Difference-between-two-dates.php

--- Difference-between-two-dates.php
+++ Difference-between-two-dates.php
@@ -1,9 +1,12 @@
 <?php
 
-date1=date_create("2013-03-15");
+$date1=date_create("2013-03-15");
 $date2=date_create("2013-04-05");
 $diff=date_diff($date1,$date2);
 echo $diff->format("%d days");
 $diff->format("%a days");
+
+//To get the current date
+echo "Today is " . date("Y-m-d") . "<br>";
 
 ?>
